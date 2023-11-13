bca02fcac3c5a6b0b4e23f9155d3528c881a5c36
Sazeduzzaman <sazeduzzaman@yahoo.com>
2023-11-13 09:32:52 +0600

    All Route Create Done

---
+++ database/seeders/SalesTeamTargetsSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class SalesTeamTargetsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
--- routes/admin.php
+++ routes/admin.php
@@ -3,21 +3,32 @@
 use App\Models\Admin\ProductTwo;
 use Illuminate\Support\Facades\Route;
 use App\Models\Admin\LeaveApplications;
+use App\Http\Controllers\Admin\FAQController;
+use App\Http\Controllers\Admin\SeoController;
 use App\Http\Controllers\Admin\BrandController;
 use App\Http\Controllers\Admin\SmtpsController;
 use App\Http\Controllers\Admin\StepsController;
 use App\Http\Controllers\Admin\ColorsController;
+use App\Http\Controllers\Admin\EventsController;
 use App\Http\Controllers\Admin\BankingsController;
 use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\IndustryController;
+use App\Http\Controllers\Admin\AddressesController;
 use App\Http\Controllers\Admin\AttributeController;
 use App\Http\Controllers\Admin\CompaniesController;
 use App\Http\Controllers\Admin\VatAndTaxController;
+use App\Http\Controllers\Admin\HrPoliciesController;
+use App\Http\Controllers\Admin\NewsTrendsController;
 use App\Http\Controllers\Admin\ProductTwoController;
 use App\Http\Controllers\KPI\ManagementKpiController;
+use App\Http\Controllers\Admin\DynamicCategorieController;
 use App\Http\Controllers\Admin\EmployeeCategoryController;
+use App\Http\Controllers\Admin\SalesTeamTargetsController;
+use App\Http\Controllers\Admin\SalesYearTargetsController;
+use App\Http\Controllers\Admin\TermsAndPoliciesController;
 use App\Http\Controllers\Admin\LeaveApplicationsController;
 use App\Http\Controllers\Admin\EmployeeDepartmentController;
+use App\Http\Controllers\Admin\PolicyAcknowledgmentsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -58,21 +69,45 @@ Route::prefix('administrator')->group(static function () {
         Route::get('/dashboard', [\App\Http\Controllers\Admin\HomeController::class, 'index'])->name('admin.index');
         Route::get('profile', [\App\Http\Controllers\Admin\HomeController::class, 'profile'])->middleware('password.confirm.admin')->name('admin.profile');
         Route::resources([
-            'vat-tax' => VatAndTaxController::class,
-            'employee-category' => EmployeeCategoryController::class,
-            'employee-department' => EmployeeDepartmentController::class,
-            'category' => CategoryController::class,
-            'brand' => BrandController::class,
-            'attribute' => AttributeController::class,
-            'colors' => ColorsController::class,
-            'smtps' => SmtpsController::class,
-            'products' => StepsController::class,
-            'industry' => IndustryController::class,
-            'product-two' => ProductTwoController::class,
-            'companies' => CompaniesController::class,
-            'leave-application' => LeaveApplicationsController::class,
-            'banking' => BankingsController::class,
-            'management-kpi' => ManagementKpiController::class,
+            'vat-tax'              =>   VatAndTaxController::class,
+            'employee-category'    =>   EmployeeCategoryController::class,
+            'employee-department'  =>   EmployeeDepartmentController::class,
+            'category'             =>   CategoryController::class,
+            'brand'                =>   BrandController::class,
+            'attribute'            =>   AttributeController::class,
+            'colors'               =>   ColorsController::class,
+            'smtps'                =>   SmtpsController::class,
+            'products'             =>   StepsController::class,
+            'industry'             =>   IndustryController::class,
+            'product-two'          =>   ProductTwoController::class,
+            'companies'            =>   CompaniesController::class,
+            'leave-application'    =>   LeaveApplicationsController::class,
+            'banking'              =>   BankingsController::class,
+            'management-kpi'       =>   ManagementKpiController::class,
+
+
+            'seo'                  =>   SeoController::class,
+            'addresse'             =>   AddressesController::class,
+            'dynamic-categorie'    =>   DynamicCategorieController::class,
+            'event'                =>   EventsController::class,
+            'faq'                  =>   FAQController::class,
+            'sales-year-target'    =>   SalesYearTargetsController::class,
+            'sales-team-target'    =>   SalesTeamTargetsController::class,
+            'news-trend'           =>   NewsTrendsController::class,
+            'hr-policie'           =>   HrPoliciesController::class,
+            'policy-acknowledgment'=>   PolicyAcknowledgmentsController::class,
+            'terms-policies'       =>   TermsAndPoliciesController::class,
+
+
+
+
+
+
+
+
+
+
+
         ]);
     });
 });
